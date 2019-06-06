1ed6c6525ef9b4cb92fbd62a39e7f96dbfa18120
Naveen Kalva <knaveen1403@gmail.com>
2019-06-06 08:52:39 +0530

    Updates

---
+++ precache-manifest.8119b8ccdc6477da72ea35d79ad825b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e672d5ca41b8c43004f9cf522de88137",
+    "url": "/roboFriends/index.html"
+  },
+  {
+    "revision": "718017f53a797179e00c",
+    "url": "/roboFriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "476b9c340d5b2de9218d",
+    "url": "/roboFriends/static/css/main.b46d3ec1.chunk.css"
+  },
+  {
+    "revision": "718017f53a797179e00c",
+    "url": "/roboFriends/static/js/2.45d95b4b.chunk.js"
+  },
+  {
+    "revision": "476b9c340d5b2de9218d",
+    "url": "/roboFriends/static/js/main.9f83d8e3.chunk.js"
+  },
+  {
+    "revision": "0d6209f39b04744aca3d",
+    "url": "/roboFriends/static/js/runtime~main.0e8c0065.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/roboFriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
